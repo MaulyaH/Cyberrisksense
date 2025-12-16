97d5d548bd551bafb71ff4facd9908b37bc623a6
Maulya H <maulyahemakumar2005@gmail.com>
2025-12-16 14:30:27 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,9 @@
 <?php
-// server/db.php
-// Edit these to match your local MySQL settings
+/
 $DB_HOST = '127.0.0.1';
 $DB_NAME = 'cyberrisksense';
 $DB_USER = 'root';
-$DB_PASS = ''; // set your mysql root password
+$DB_PASS = '';
 
 try {
     $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4", $DB_USER, $DB_PASS, [
